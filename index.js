d677c56e160c4949699c73a23bb3ec36651bc3fd
aowuor <angelaowuor@gmail.com>
2022-11-21 12:02:26 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+const currentUser = "Angela";
+let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+
+const upperCaseCurrentUser = currentUser.toUpperCase();
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + upperCaseCurrentUser + "!";
+
+const shortGreeting = `Welcome, ${currentUser.charAt(0)}!`;
+
